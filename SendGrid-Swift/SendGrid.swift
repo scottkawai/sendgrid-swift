97a52f346a04850e11324e130a27ecf0b03fff5d
Scott K <scott.kawai@sendgrid.com>
2014-10-26 14:59:45 -0700

    Moved to a SendGrid/Email class model like other libraries

--- SendGrid-Swift/SendGrid.swift
+++ SendGrid-Swift/SendGrid.swift
@@ -22,22 +22,6 @@ class SendGrid {
     let username: String
     let password: String
     
-    var to: [String]?
-    var toname: [String]?
-    var subject: String?
-    var from: String?
-    var fromname: String?
-    var text: String?
-    var html: String?
-    var headers: [String:String]?
-    var cc: [String]?
-    var bcc: [String]?
-    var replyto: String?
-    
-    let smtpapi = SmtpApi()
-    var hasRecipientsInSmtpApi = true
-    
-    
     // MARK: INITIALIZATION
     //=========================================================================
     
@@ -46,135 +30,10 @@ class SendGrid {
         self.password = password;
     }
     
-    
     // MARK: FUNCTIONS
     //=========================================================================
     
-    func addTo(address: String, name: String?) {
-        var names: [String]?
-        if let n = name {
-            names = [n]
-        }
-        self.addTos([address], names: names)
-    }
-    
-    func addTos(addresses: [String], names: [String]?) {
-        if self.hasRecipientsInSmtpApi {
-            self.smtpapi.addTos(addresses, names: names)
-        } else {
-            if self.to == nil {
-                self.to = []
-            }
-            
-            if let toNames = names {
-                if self.toname == nil {
-                    self.toname = []
-                }
-                if countElements(addresses) == countElements(toNames) {
-                    self.toname! += toNames
-                } else {
-                    println("[**ERROR**] SendGrid addTos: The number of email addresses provided didn't match the number of names provided.")
-                    return
-                }
-            }
-            
-            self.to! += addresses
-        }
-    }
-    
-    func setTos(addresses: [String], names: [String]?) {
-        if self.hasRecipientsInSmtpApi {
-            self.smtpapi.setTos(addresses, names: names)
-        } else {
-            self.to = nil
-            self.toname = nil
-            self.addTos(addresses, names: names)
-        }
-    }
-    
-    func setSubject(subject: String) {
-        self.subject = subject
-    }
-    
-    func setFrom(address: String, name: String?) {
-        self.from = address
-        self.fromname = name
-    }
-    
-    func setReplyTo(address: String) {
-        self.replyto = address
-    }
-    
-    func addCC(address: String) {
-        self.addCCs([address])
-    }
-    
-    func addCCs(addresses: [String]) {
-        if self.cc != nil {
-            self.cc! += addresses
-        } else {
-            self.cc = addresses
-        }
-    }
-    
-    func setCCs(addresses: [String]) {
-        self.cc = nil
-        self.addCCs(addresses)
-    }
-    
-    func addBCC(address: String) {
-        self.addBCCs([address])
-    }
-    
-    func addBCCs(addresses: [String]) {
-        if self.hasRecipientsInSmtpApi {
-            Logger.warn("The BCC option will not work when specifying recipients in the X-SMTPAPI header. Set the `hasRecipientsInSmtpApi` to false before adding BCC addresses so that the 'To' addresses get added to the normal 'To' header instead of the X-SMTPAPI header.")
-        }
-        if self.bcc != nil {
-            self.bcc! += addresses
-        } else {
-            self.bcc = addresses
-        }
-    }
-    
-    func setBCCs(addresses: [String]) {
-        self.bcc = nil
-        self.addBCCs(addresses)
-    }
-    
-    func setTextBody(text: String) {
-        self.text = text
-    }
-    
-    func setHtmlBody(html: String) {
-        self.html = html
-    }
-    
-    func setBody(text: String, html: String) {
-        self.setTextBody(text)
-        self.setHtmlBody(html)
-    }
-    
-    func addHeader(key: String, value: String) {
-        self.addHeaders([key: value])
-    }
-    
-    func addHeaders(keyValuePairs: [String:String]) {
-        if self.headers == nil {
-            self.headers = keyValuePairs
-        } else {
-            for (key, value) in keyValuePairs {
-                self.headers![key] = value
-            }
-        }
-    }
-    
-    func setHeaders(keyValuePairs: [String:String]) {
-        self.headers = nil
-        self.addHeaders(keyValuePairs)
-    }
-    
-    func send(completionHandler: ((NSURLResponse!, NSData!, NSError!) -> Void)?) {
+    func send(email: SendGrid.Email, completionHandler: ((NSURLResponse!, NSData!, NSError!) -> Void)?) {
         let url = NSURL(string: "https://api.sendgrid.com/api/mail.send.json")!
         let request = NSMutableURLRequest(URL: url)
         request.HTTPMethod = "POST"
@@ -197,35 +56,35 @@ class SendGrid {
             return false
         }
         
-        if !addToParamsIfPresent("from", self.from) {
+        if !addToParamsIfPresent("from", email.from) {
             Logger.error("SendGrid send: Could not send message due to required `from` parameter missing.")
             return
         }
         
-        addToParamsIfPresent("fromname", self.fromname)
+        addToParamsIfPresent("fromname", email.fromname)
         
-        if self.hasRecipientsInSmtpApi && self.smtpapi.to != nil && countElements(self.smtpapi.to!) > 0 {
-            params["to"] = self.from!
-        } else if !self.hasRecipientsInSmtpApi && self.to != nil && countElements(self.to!) > 0 {
-            params["to"] = self.to!
+        if email.hasRecipientsInSmtpApi && email.smtpapi.to != nil && countElements(email.smtpapi.to!) > 0 {
+            params["to"] = email.from!
+        } else if !email.hasRecipientsInSmtpApi && email.to != nil && countElements(email.to!) > 0 {
+            params["to"] = email.to!
         } else {
             Logger.error("SendGrid send: Could not send message as no recipients were specified.")
             return
         }
         
-        addToParamsIfPresent("toname", self.toname)
+        addToParamsIfPresent("toname", email.toname)
         
-        if !addToParamsIfPresent("subject", subject) {
+        if !addToParamsIfPresent("subject", email.subject) {
             Logger.error("SendGrid send: Could not send message - a subject is required.")
             return
         }
         
-        addToParamsIfPresent("text", self.text)
-        addToParamsIfPresent("html", self.html)
-        addToParamsIfPresent("cc", self.cc)
-        addToParamsIfPresent("bcc", self.bcc)
-        addToParamsIfPresent("replyto", self.replyto)
-        addToParamsIfPresent("headers", self.headers)
+        addToParamsIfPresent("text", email.text)
+        addToParamsIfPresent("html", email.html)
+        addToParamsIfPresent("cc", email.cc)
+        addToParamsIfPresent("bcc", email.bcc)
+        addToParamsIfPresent("replyto", email.replyto)
+        addToParamsIfPresent("headers", email.headers)
         
         var body = ""
         for (paramName, paramValue) in params {
@@ -258,8 +117,8 @@ class SendGrid {
         }
         
         // SMTPAPI
-        if self.smtpapi.hasSmtpApi {
-            body += "&x-smtpapi=" + self.smtpapi.jsonValue.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        if email.smtpapi.hasSmtpApi {
+            body += "&x-smtpapi=" + email.smtpapi.jsonValue.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
         }
         
         println(body)
@@ -271,5 +130,159 @@ class SendGrid {
         //                handler(response, data, error)
         //            }
         //        }
+    }
+    
+    
+    class Email {
+        
+        // MARK: PROPERTIES
+        //=========================================================================
+        
+        var to: [String]?
+        var toname: [String]?
+        var subject: String?
+        var from: String?
+        var fromname: String?
+        var text: String?
+        var html: String?
+        var headers: [String:String]?
+        var cc: [String]?
+        var bcc: [String]?
+        var replyto: String?
+        
+        let smtpapi = SmtpApi()
+        var hasRecipientsInSmtpApi = true
+        
+        // MARK: INITIALIZATION
+        //=========================================================================
+        
+        init() {}
+        
+        // MARK: FUNCTIONS
+        //=========================================================================
+        
+        func addTo(address: String, name: String?) {
+            var names: [String]?
+            if let n = name {
+                names = [n]
+            }
+            self.addTos([address], names: names)
+        }
+        
+        func addTos(addresses: [String], names: [String]?) {
+            if self.hasRecipientsInSmtpApi {
+                self.smtpapi.addTos(addresses, names: names)
+            } else {
+                if self.to == nil {
+                    self.to = []
+                }
+                
+                if let toNames = names {
+                    if self.toname == nil {
+                        self.toname = []
+                    }
+                    if countElements(addresses) == countElements(toNames) {
+                        self.toname! += toNames
+                    } else {
+                        println("[**ERROR**] SendGrid addTos: The number of email addresses provided didn't match the number of names provided.")
+                        return
+                    }
+                }
+                
+                self.to! += addresses
+            }
+        }
+        
+        func setTos(addresses: [String], names: [String]?) {
+            if self.hasRecipientsInSmtpApi {
+                self.smtpapi.setTos(addresses, names: names)
+            } else {
+                self.to = nil
+                self.toname = nil
+                self.addTos(addresses, names: names)
+            }
+        }
+        
+        func setSubject(subject: String) {
+            self.subject = subject
+        }
+        
+        func setFrom(address: String, name: String?) {
+            self.from = address
+            self.fromname = name
+        }
+        
+        func setReplyTo(address: String) {
+            self.replyto = address
+        }
+        
+        func addCC(address: String) {
+            self.addCCs([address])
+        }
+        
+        func addCCs(addresses: [String]) {
+            if self.cc != nil {
+                self.cc! += addresses
+            } else {
+                self.cc = addresses
+            }
+        }
+        
+        func setCCs(addresses: [String]) {
+            self.cc = nil
+            self.addCCs(addresses)
+        }
+        
+        func addBCC(address: String) {
+            self.addBCCs([address])
+        }
+        
+        func addBCCs(addresses: [String]) {
+            if self.hasRecipientsInSmtpApi {
+                Logger.warn("The BCC option will not work when specifying recipients in the X-SMTPAPI header. Set the `hasRecipientsInSmtpApi` to false before adding BCC addresses so that the 'To' addresses get added to the normal 'To' header instead of the X-SMTPAPI header.")
+            }
+            if self.bcc != nil {
+                self.bcc! += addresses
+            } else {
+                self.bcc = addresses
+            }
+        }
+        
+        func setBCCs(addresses: [String]) {
+            self.bcc = nil
+            self.addBCCs(addresses)
+        }
+        
+        func setTextBody(text: String) {
+            self.text = text
+        }
+        
+        func setHtmlBody(html: String) {
+            self.html = html
+        }
+        
+        func setBody(text: String, html: String) {
+            self.setTextBody(text)
+            self.setHtmlBody(html)
+        }
+        
+        func addHeader(key: String, value: String) {
+            self.addHeaders([key: value])
+        }
+        
+        func addHeaders(keyValuePairs: [String:String]) {
+            if self.headers == nil {
+                self.headers = keyValuePairs
+            } else {
+                for (key, value) in keyValuePairs {
+                    self.headers![key] = value
+                }
+            }
+        }
+        
+        func setHeaders(keyValuePairs: [String:String]) {
+            self.headers = nil
+            self.addHeaders(keyValuePairs)
+        }
     }
 }
