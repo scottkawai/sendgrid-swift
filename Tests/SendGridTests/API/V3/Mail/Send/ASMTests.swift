cfe38584a8d847e88f1e66a0a79a03d9dce78c66
Scott K <scott@sendgrid.com>
2017-09-17 14:26:48 -0700

    Add missing ASM tests

--- Tests/SendGridTests/API/V3/Mail/Send/ASMTests.swift
+++ Tests/SendGridTests/API/V3/Mail/Send/ASMTests.swift
@@ -20,4 +20,24 @@ class ASMTests: XCTestCase, EncodingTester {
         XCTAssertEncodedObject(max, equals: ["group_id": 4, "groups_to_display": [8, 15, 16, 23, 42]])
     }
     
+    func testInitialization() {
+        let basic = ASM(groupID: 4)
+        XCTAssertEqual(basic.id, 4)
+        XCTAssertNil(basic.groupsToDisplay)
+        
+        let advance = ASM(groupID: 4, groupsToDisplay: [8, 15, 16, 23, 42])
+        XCTAssertEqual(advance.id, 4)
+        XCTAssertEqual(advance.groupsToDisplay!, [8, 15, 16, 23, 42])
+    }
+    
+    func testValidation() {
+        do {
+            let over = ASM(groupID: 815, groupsToDisplay: Array(1...30))
+            try over.validate()
+            XCTFail("Expected an error to be thrown when `ASM` is provided more than \(Constants.UnsubscribeGroups.MaximumNumberOfDisplayGroups) groups to display, but nothing was thrown.")
+        } catch {
+            XCTAssertEqual("\(error)", SendGrid.Exception.Mail.tooManyUnsubscribeGroups.description)
+        }
+    }
+    
 }
