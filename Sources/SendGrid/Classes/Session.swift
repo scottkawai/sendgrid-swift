9bbc6dcd00d7882bba6b6131e06ccbf26a3eb558
Scott K <scott@sendgrid.com>
2017-09-19 08:48:44 -0700

    Don’t use shared URLSession

--- Sources/SendGrid/Classes/Session.swift
+++ Sources/SendGrid/Classes/Session.swift
@@ -75,7 +75,7 @@ open class Session {
         if let sub = subuser { payload.addValue(sub, forHTTPHeaderField: "On-behalf-of") }
         
         // Make the HTTP request
-        let task = URLSession.shared.dataTask(with: payload) { (data, response, error) in
+        let task = self.urlSession.dataTask(with: payload) { (data, response, error) in
             let resp = Response<ModelType>(data: data, response: response, error: error, decodingStrategy: request.decodingStrategy)
             completionHandler(resp)
         }
