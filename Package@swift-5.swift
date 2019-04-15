bf1d2c02b8152e5f5d703e791ac9fe7bdeb04593
Ron Pinz <ron.pinz@gmail.com>
2019-04-15 15:07:01 -0400

    update manifests for 4.1/4.2/5.0

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,15 +8,21 @@ let package = Package(
     products: [
         .library(
             name: "SendGrid",
-            targets: ["SendGrid"]),
+            targets: ["SendGrid"]
+        )
     ],
     dependencies: [],
     targets: [
         .target(
             name: "SendGrid",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SendGridTests",
-            dependencies: ["SendGrid"]),
+            dependencies: ["SendGrid"]
+        )
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
